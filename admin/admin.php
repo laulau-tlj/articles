cbcf9fd1ebe2b85a9066061f3362781e2e2dc7b1
laulau-tlj <76941580+laulau-tlj@users.noreply.github.com>
2023-07-20 21:04:18 +0200

    update admin file

--- admin/admin.php
+++ admin/admin.php
@@ -64,14 +64,90 @@ include_once('includes/header.php');
             <?php
             // sinon si action correspond à update_delete
         }elseif ($_GET['action'] == 'update_delete') {
-            
-            $req_all_article
-            // On se connexte à la bdd
+            // Connexion à la bdd
             require_once('includes/database.php');
-            // Si on clique sur le bouton update on affiche le formulaire de modification
-            if (isset($_POST['update'])) {
+            // On récupère les articles de la table
+            $req_all_article = $db->prepare('SELECT * FROM  all_articles');
+            // On execute la requêtte
+            $req_all_article->execute(); ?>
+            <table class="table align-middle mb-0 bg-white">
+                <thead class="bg-light">
+                    <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                    </tr>
+                </thead>
+            <?php
+            // Boucle while qui va afficher le contenue de la table
+            while ($reponse = $req_all_article->fetch(PDO::FETCH_OBJ)) {
+            ?>
+
+                <tr>
+                    <td>
+                        <?php echo $reponse->image_name ?>
+                    </td>
+                    <td>
+                        <?php echo $reponse->titre ?>
+                    </td>
+                    <td>
+                        <a href="?action=update&id=<?php echo $reponse->id ?>">Update</a>
+                    </td>
+                    <td>
+                        <a href="?action=delete&id=<?php echo $reponse->id ?>">Delete</a>
+                    </td>
+                </tr>
+
+            <?php
+            }  ?>
+            </table>
+<?php
+        }
+        // sinon si action correspond à delete
+        elseif ($_GET['action'] == 'delete') {
+            // Connexion à la bdd
+            require_once('includes/database.php');
+            // On récupère les articles de la table
+            $delete_art = $db->prepare('DELETE FROM all_articles WHERE id=?');
+            // On execute la requêtte
+            $delete_art->execute(array($_GET['id']));
+            // Redirection vers la page admin
+            header('location:admin.php?action=update_delete');
+
+        }// sinon si action correspond à update
+        elseif ($_GET['action'] == 'update') {
+            // Connexion à la bdd
+            require_once('includes/database.php');
+            // Si on soumet le formulaire de modification
+            if (isset($_POST['submit'])) {
+                // Extraire le contenu de la variable $_POST
+                extract($_POST);
+                // Requêtte de modofication
+                $update_art = $db->prepare('UPDATE all_articles set titre=? and description=? WHERE id=?');
+                // On execute la requêtte
+                $update_art->execute(array($titre,$description,$_GET['id']));
+                // On redirige
+                header('location:admin.php?action=update_delete');
 
             }
+            // On récupère les articles de la table
+            $get_art = $db->prepare('SELECT * FROM all_articles WHERE id=?');
+            // On execute la requêtte
+            $get_art->execute(array($_GET['id']));
+
+            $reponses = $get_art->fetch(PDO::FETCH_OBJ);
+            
+            ?>
+            <h3>Modifier un article</h3><br>
+            <form method="POST" action="" style="border: #D3D3D3 solid 1px; padding: 3%; border-radius: 5px">
+                <input type="text" name="titre" value="<?php echo $reponses->titre?>" required class="form form-control"><br>
+                <textarea name="description" class="form form-control" required><?php echo $reponses->description?></textarea><br>
+                <input type="submit" name="submit" class="btn btn-primary">
+            </form>
+
+
+<?php
         }
     }
 
